bc2a9e341c1d76d2df03456efe9d9e4a79ff763e
Erik Hommel <hommel@ee-atwork.nl>
2014-11-27 15:01:50 +0100

    BOS1408772 set time limit

--- api/v3/AnnaZeepsop/ProcessFirstDel.php
+++ api/v3/AnnaZeepsop/ProcessFirstDel.php
@@ -12,6 +12,7 @@
  * @see civicrm_api3_create_error
  * @throws API_Exception
  */
+set_time_limit(0);
 function civicrm_api3_anna_zeepsop_processfirstdel($params) {
   $custom_id = get_custom_field_persoonsnummer_first();
   define('PERSOONSNUMMER_CUSTOM_ID', $custom_id);
