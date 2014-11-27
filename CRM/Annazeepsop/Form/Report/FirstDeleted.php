caaf1b6118e8153147eb555783142ea4b6401ac4
Erik Hommel <hommel@ee-atwork.nl>
2014-11-27 15:15:02 +0100

    BOS1408772 alter date

--- CRM/Annazeepsop/Form/Report/FirstDeleted.php
+++ CRM/Annazeepsop/Form/Report/FirstDeleted.php
@@ -109,10 +109,12 @@ class CRM_Annazeepsop_Form_Report_FirstDeleted extends CRM_Report_Form {
   }
   
   protected function alter_date($in_date) {
+    CRM_Core_Error::debug('in_date', $in_date);
     $out_date = '';
     if (!empty($in_date) && $in_date != '1970-01-01') {
       $out_date = date('d-m-Y', strtotime($in_date));        
     }
+    CRM_Core_Error::debug('out_date', $out_date);
     return $out_date;
   }
   
